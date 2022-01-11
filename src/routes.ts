e658d3e36b4ee2931477781e38dbae63e5b08886
Diego Goulart <diegogoulart.aws@gmail.com>
2022-01-11 12:17:27 -0300

    feat: Create Deliveryman use case.

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
 routes.post("/clients/", createClientController.handle);
 routes.post("/clients/auth/", authenticateClientController.handle);
+
+routes.post("/deliverers/", createDeliverymanController.handle);
 
 export { routes };
